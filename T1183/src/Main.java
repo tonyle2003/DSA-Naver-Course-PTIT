6cb5dd00b9715e6995a0ff02d4ab4bc72f12952f
tonyle2003 <laptrinhcungtony@gmail.com>
2023-03-06 12:53:54 +0700

    TLE

--- T1183/src/Main.java
+++ T1183/src/Main.java
@@ -1,56 +1,9 @@
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Vector;
 
-class Graph {
-
-    private Vector<Vector<Integer>> adjacency;
-    private boolean[] visited;
-    private Vector<Integer> path;
-
-    public Graph(int v) {
-        path = new Vector<>();
-        adjacency  = new Vector<>();
-        visited = new boolean[v + 1];
-        for (int i = 0; i <= v; i++) {
-            adjacency .add(new Vector<>());
-            visited[i] = false;
-        }
-    }
-
-    public void add(int first, int second) {
-        adjacency .get(first).add(second);
-    }
-
-    public void depthFirstSearchPath(int begin, int end) {
-        if (depthFirstSearch(begin, end) == false) {
-            System.out.print(-1);
-        } else {
-            depthFirstSearch(begin, end);
-            for (Integer element : path) {
-                System.out.print(element + " ");
-            }
-            Arrays.fill(visited, false);
-        }
-    }
-
-    private boolean depthFirstSearch(int begin, int end) {
-        if (visited[end] == true) {
-            return true;
-        }
-        visited[begin] = true;
-        path.add(begin);
-        for (Integer element : adjacency .get(begin)) {
-            if (visited[element] == false) {
-                depthFirstSearch(element, end);
-                if (visited[end] == true) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-}
 
 public class Main {
 
@@ -59,23 +12,72 @@ public class Main {
         int test = scanner.nextInt();
         while (test > 0) {
             test--;
-
-            int v = scanner.nextInt();
-            int e = scanner.nextInt();
+            int vertices = scanner.nextInt();
+            int edges = scanner.nextInt();
             int begin = scanner.nextInt();
             int end = scanner.nextInt();
-
-            Graph myGraph = new Graph(v);
-
-            for (int i = 1; i <= e; i++) {
+            
+            Graph graph = new Graph(vertices);
+            for (int i = 1; i <= edges; i++) {
                 int first = scanner.nextInt();
                 int second = scanner.nextInt();
-                myGraph.add(first, second);
-                myGraph.add(second, first);
+                graph.add(first, second);
+                graph.add(second, first);
             }
-            myGraph.depthFirstSearchPath(begin, end);
-            System.out.println();
+            graph.depthFirstSearchPath(begin, end);
         }
         scanner.close();
+    }
+}
+
+class Graph {
+
+    private Vector<Vector<Integer>> adjacency;
+    private boolean[] visited;
+    private Deque<Integer> deque;
+
+    public Graph(int vertices) {
+        adjacency = new Vector<>();
+        visited = new boolean[vertices + 1];
+        deque = new LinkedList<>();
+        for (int i = 0; i <= vertices; i++) {
+            adjacency.add(new Vector<>());
+            visited[i] = false;
+        }
+    }
+
+    public void add(int begin, int end) {
+        adjacency.get(begin).add(end);
+    }
+
+    public void depthFirstSearchPath(int begin, int end) {
+        if (depthFirstSearch(begin, end) == true) {
+            while (deque.isEmpty() == false) {
+                System.out.print(deque.pollFirst() + " ");
+            }
+            System.out.println();
+        } else {
+            System.out.println(-1);
+        }
+        Arrays.fill(visited, false);
+    }
+
+    private boolean depthFirstSearch(int begin, int end) {
+        if (visited[end] == true) {
+            return true;
+        }
+        deque.addLast(begin);
+        visited[begin] = true;
+        for (int element : adjacency.get(begin)) {
+            if (visited[element] == false) {
+                depthFirstSearch(element, end);
+                if (visited[end] ==  true) {
+                    return true;
+                }
+            }
+        }
+        visited[begin] = false;
+        deque.pollLast();
+        return false;
     }
 }
